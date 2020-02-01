8aa15a0566846b7960c758bd5f6f61314dc7e5fe
Luis Geniole <luis.geniole@gmail.com>
2020-02-01 11:50:38 -0300

    Install all packages and configure linting

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    camelcase: "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+};
